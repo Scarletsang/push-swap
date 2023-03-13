fda247ee5fdadaaf6613632029d9ed0fb2495e29
Anthony Tsang <htsang@student.42heilbronn.de>
2023-03-13 23:41:41 +0100

    test: Each test is now grouped under a function

--- src/main.c
+++ src/main.c
@@ -6,17 +6,14 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 21:29:34 by htsang            #+#    #+#             */
-/*   Updated: 2023/03/11 23:36:05 by htsang           ###   ########.fr       */
+/*   Updated: 2023/03/13 23:15:30 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PUSH_SWAP/push_swap.h"
 
-void	test(t_push_swap_2stacks *two_stacks)
+void	test_stack_operations(t_push_swap_2stacks *two_stacks)
 {
-	// print_stack_info(&two_stacks->stack_a);
-	// print_stack_elements(&two_stacks->stack_a);
-	// rotate(&sorter.stack_a);
 	print_two_stacks(two_stacks);
 	rotate(&two_stacks->stack_a);
 	print_two_stacks(two_stacks);
@@ -26,17 +23,22 @@ void	test(t_push_swap_2stacks *two_stacks)
 	print_two_stacks(two_stacks);
 	push(&two_stacks->stack_a, &two_stacks->stack_b);
 	print_two_stacks(two_stacks);
-	// print_stack_info(&sorter->stack_a);
-	// print_stack_elements(&sorter->stack_a);
-	// print_stacks(&sorter);
-	// reverse_rotate(&sorter.stack_a);
-	// print_stacks(&sorter);
-	// push(&sorter.stack_b, &sorter.stack_a);
-	// print_stacks(&sorter);
-	// push(&sorter.stack_b, &sorter.stack_a);
-	// print_stacks(&sorter);
-	free(two_stacks->stack_a.elements);
-	free(two_stacks->stack_b.elements);
+}
+
+void	test_emulation_indexing(t_push_swap_2stacks *two_stacks, \
+t_push_swap_sorter *sorter)
+{
+	emulate_two_stacks(sorter, two_stacks, RANGE_FROM_MINUS_1, TRIANGLE_SIZE_5);
+	print_stack_elements(&sorter->emulation.stack_a);
+	print_stack_elements(&two_stacks->stack_a);
+}
+
+void	test_naive_triangle_creation(t_push_swap_2stacks *two_stacks, \
+t_push_swap_sorter *sorter)
+{
+	sort_size_of(sorter, two_stacks, two_stacks->stack_a.size);
+	print_instructions(sorter);
+	print_two_stacks(two_stacks);
 }
 
 int	init_program(t_push_swap_2stacks *two_stacks, int argc, \
@@ -65,11 +67,11 @@ int	main(int argc, const char **argv)
 		return (EXIT_FAILURE);
 	}
 	ft_printf("-----------\n");
-	sort_size_of(&sorter, &two_stacks, two_stacks.stack_a.size);
-	print_instructions(&sorter);
-	clean_sorter(&sorter);
-	print_two_stacks(&two_stacks);
+	// test_naive_triangle_creation(&two_stacks, &sorter);
+	test_emulation_indexing(&two_stacks, &sorter);
 	// print_all_triangles_merge(atoi(argv[1]));
-	// test(&two_stacks);
+	// test_stack_operations(&two_stacks);
+	free_sorter_internals(&sorter);
+	free_two_stacks(&two_stacks);
 	return (EXIT_SUCCESS);
 }
