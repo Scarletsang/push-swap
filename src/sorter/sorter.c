bc73e6dc8920638b722b8b348c765e1ed64eece1
Anthony Tsang <anthonytsang0226@gmail.com>
2023-03-22 04:41:44 +0100

    fix(sorter): create_all_triangles leaks

--- src/sorter/sorter.c
+++ src/sorter/sorter.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 21:38:05 by htsang            #+#    #+#             */
-/*   Updated: 2023/03/22 00:03:45 by htsang           ###   ########.fr       */
+/*   Updated: 2023/03/22 04:30:02 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,20 @@ t_push_swap_triangles_planner *planner, t_push_swap_instructor *instructor)
 	triangle_index = 0;
 	while (triangle_index < planner->total_triangles)
 	{
-		prepare_emulation(&triangle_maker, \
-			planner->triangles[triangle_index], \
-			get_triangle_shape(planner->total_triangles - 1 - triangle_index, \
-				planner->total_triangles));
+		prepare_emulation(&triangle_maker, planner->triangles[triangle_index], \
+			get_triangle_shape(triangle_index, planner->total_triangles));
 		emulate_two_stacks(&triangle_maker, two_stacks);
-		print_instructions(&emulation_instructor);
-		print_two_stacks(&triangle_maker.emulation);
 		if (create_triangle(&emulation_instructor, &triangle_maker))
 		{
 			free_instruction_list(emulation_instructor.cost);
 			free_two_stacks(&triangle_maker.emulation);
 			return (FAILURE);
 		}
-		print_instructions(&emulation_instructor);
-		print_two_stacks(&triangle_maker.emulation);
 		concat_instructor(instructor, &emulation_instructor);
 		execute_unexecuted_instructions(instructor);
 		triangle_index++;
 	}
+	free(emulation_instructor.cost);
 	free_two_stacks(&triangle_maker.emulation);
 	return (SUCCESS);
 }
